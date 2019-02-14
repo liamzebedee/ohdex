b6aa46b4ae8e02ec0c45ac2065a533929f006691
Mick de Graaf <mick@mickdegraaf.nl>
2019-02-14 15:29:11 +0100

    finished create token

---
+++ contracts/contracts/CustomToken.sol
@@ -0,0 +1,18 @@
+pragma solidity ^0.5.0;
+
+import "openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
+
+contract CustomToken is ERC20 {
+
+    string public name;
+    string public symbol;
+    uint8 public decimals = 18;
+
+    constructor(string memory _name, string memory _symbol, uint256 _initialSupply) public {
+        name = _name;
+        symbol = _symbol;
+        _mint(msg.sender, _initialSupply);
+    }
+
+
+}
