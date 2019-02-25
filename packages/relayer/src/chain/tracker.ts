245aead81dd2ce3f2b151f341f064dea3351ef52
liamzebedee <liamzebedee@yahoo.com.au>
2019-02-25 13:51:23 +0100

    Fix event bug

--- packages/relayer/src/chain/tracker.ts
+++ packages/relayer/src/chain/tracker.ts
@@ -4,14 +4,18 @@ import { IChain } from "../../../multichain/lib/types";
 import { ITokenBridgeEventArgs } from "../../../contracts/build/wrappers/i_token_bridge";
 const eventEmitter = require("events");
 
-interface EventEmittedEvent extends Event {
+
+import Event from 'events'
+
+
+interface EventEmittedEvent {
     eventHash: string;
     newChainRoot: string;
     newChainIndex: string;
 }
 
 type chainId = string
-interface MessageSentEvent extends Event {
+interface MessageSentEvent {
     fromChain: chainId;
     toBridge: string;
     data: ITokenBridgeEventArgs;
